d8fed12243146dd5b318102c894831bd129a6fe6
rodriquelali <rodriquelali@gmail.com>
2024-07-19 09:25:16 -0400

    blade primeros pasos

--- example-app/resources/views/body/cuerpo.blade.php
+++ example-app/resources/views/body/cuerpo.blade.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>@yield('title')</title>
 </head>
 <body>
-    <h1>BIENVENIDO AL SISTEMA......</h1>
+
+    @yield('cuerpo')
+
 </body>
 </html>
---
+++ example-app/resources/views/producto/verProducto.blade.php
@@ -0,0 +1,7 @@
+@extends('body.cuerpo')
+
+@section('title', 'Ver Producto '.$datos)
+
+@section('cuerpo')
+    <h1>ver producto {{$datos}}</h1>
+@endsection()
