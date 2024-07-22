69f9267467a08551b380faf71fd86ea4a0ba5646
rodriquelali <94028016+rodriQuelali@users.noreply.github.com>
2024-07-22 11:43:57 -0400

    Proyecto/rodrigo (#4)
    * blade primeros pasos
    
    * manejando blade

--- example-app/resources/views/body/cuerpo.blade.php
+++ example-app/resources/views/body/cuerpo.blade.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>@yield('title')</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <h1>BIENVENIDO AL SISTEMA......</h1>
+
+    @yield('cuerpo')
+
+
+    @vite('resources/js/app.js')
 </body>
 </html>
