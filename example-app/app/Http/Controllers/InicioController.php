b4322683e9b0d7f00bcfb2a0729b8e70efff6217
rodriquelali <94028016+rodriQuelali@users.noreply.github.com>
2024-08-23 08:31:49 -0400

    login parte 2 (#11)

--- example-app/app/Http/Controllers/InicioController.php
+++ example-app/app/Http/Controllers/InicioController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 class InicioController extends Controller
 {
     //
-    public function __invoke(){
+
+    public function __construct() {
+        $this->middleware('auth');
+    }
+    public function inicio(){
         return view('inicio');
     }
 }
